8075795ad9c9aec9acb9eab8b55a9708c85c1632
suyanlong <592938410@qq.com>
2023-08-04 18:05:39 +0800

    Impl tuple decode

--- src/codec/decode/impl_tuples.rs
+++ src/codec/decode/impl_tuples.rs
@@ -64,3 +64,69 @@ impl<A, B, C, D, E, F> Decode for (A, B, C, D, E, F)
         Ok((A::decode(decoder)?, B::decode(decoder)?, C::decode(decoder)?, D::decode(decoder)?, E::decode(decoder)?, F::decode(decoder)?))
     }
 }
+
+impl<A, B, C, D, E, F, G> Decode for (A, B, C, D, E, F, G)
+    where A: Decode,
+          B: Decode,
+          C: Decode,
+          D: Decode,
+          E: Decode,
+          F: Decode,
+          G: Decode
+{
+    fn decode<DE: Decoder>(decoder: &mut DE) -> Result<Self, DecodeError> {
+        Ok((A::decode(decoder)?,
+            B::decode(decoder)?,
+            C::decode(decoder)?,
+            D::decode(decoder)?,
+            E::decode(decoder)?,
+            F::decode(decoder)?,
+            G::decode(decoder)?))
+    }
+}
+
+impl<A, B, C, D, E, F, G, H> Decode for (A, B, C, D, E, F, G, H)
+    where A: Decode,
+          B: Decode,
+          C: Decode,
+          D: Decode,
+          E: Decode,
+          F: Decode,
+          G: Decode,
+          H: Decode
+{
+    fn decode<DE: Decoder>(decoder: &mut DE) -> Result<Self, DecodeError> {
+        Ok((A::decode(decoder)?,
+            B::decode(decoder)?,
+            C::decode(decoder)?,
+            D::decode(decoder)?,
+            E::decode(decoder)?,
+            F::decode(decoder)?,
+            G::decode(decoder)?,
+            H::decode(decoder)?))
+    }
+}
+
+impl<A, B, C, D, E, F, G, H, I> Decode for (A, B, C, D, E, F, G, H, I)
+    where A: Decode,
+          B: Decode,
+          C: Decode,
+          D: Decode,
+          E: Decode,
+          F: Decode,
+          G: Decode,
+          H: Decode,
+          I: Decode
+{
+    fn decode<DE: Decoder>(decoder: &mut DE) -> Result<Self, DecodeError> {
+        Ok((A::decode(decoder)?,
+            B::decode(decoder)?,
+            C::decode(decoder)?,
+            D::decode(decoder)?,
+            E::decode(decoder)?,
+            F::decode(decoder)?,
+            G::decode(decoder)?,
+            H::decode(decoder)?,
+            I::decode(decoder)?))
+    }
+}
